98520d0951eb12b8483f4ac54f31064f28b02fe6
Ben Ye <z_ye@ucsb.edu>
2020-11-15 22:06:45 -0800

    setup complete. need to add weapons and respawn system

---
+++ Assets/Scripts/Game/System/MenuController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    //this makes us load game scene
+    public void OnPlay() {
+        SceneManager.LoadScene("Main");
+        //whenever you make games for any platform, you must
+        //add the scenes to the build settings
+
+        //in editor, the lighting might be built weirdly/
+        //make sure to disable the auto checkbox and hit 'build' in lighting under scene.
+    }
+
+    void Update() {
+        if (Input.GetKey(KeyCode.KeypadEnter) || Input.GetKey("enter") || Input.GetKey("return")) {
+            SceneManager.LoadScene("Main");
+        }
+    }
+}
